5d1ad2d548ecec1722d1f7a610d8430e5319e470
Varsha Venkatasubramanian <varsha@Varshas-MBP.lan>
2023-04-29 13:41:46 -0400

    Updating MongoDB requests and exception handling

--- main.py
+++ main.py
@@ -2,43 +2,53 @@
 ### tldr; if you paste in links to open access PDF files, this will download all the PDFs for you ###
 
 import requests
+from requests.exceptions import MissingSchema
 from pathvalidate import sanitize_filepath
 import mongoservice
 
 def check_response(user_response):
-    user_response = user_response.lower()
-    if user_response == "y" or user_response == "yes":
-        return True
-    return False
+  user_response = user_response.lower()
+  if user_response == "y" or user_response == "yes":
+    return True
+  return False
 
 def process(link):
-    # open link using GET
+  # open link using GET
+  try:
     r = requests.get(link)
     # retrieve the content type of response to check whether it is a PDF
     content_type = r.headers.get('content-type')
     to_download = False
     # if the GET request succeeded and file is a PDF
     if r.status_code == 200 and "pdf" in content_type:
-        user_response = input("This is an open access PDF file. Download? (y/n) ")
-        # check whether user response is a confirmation
-        to_download = check_response(user_response)
-        # if user has confirmed, go ahead and download the PDF
-        if to_download:
-            name = input("Enter a file name ")
-            open("pdfs/" + sanitize_filepath(name) + ".pdf", 'wb').write(r.content)
+      user_response = input(
+        "This is an open access PDF file. Download? (y/n) ")
+      # check whether user response is a confirmation
+      to_download = check_response(user_response)
+      # if user has confirmed, go ahead and download the PDF
+      if to_download:
+        name = input("Enter a file name ")
+        open("pdfs/" + sanitize_filepath(name) + ".pdf", 'wb').write(r.content)
     # if the GET request succeeded but the file is not a PDF
     elif r.status_code == 200:
-        print("This is not a PDF :(")
+      print("This is not a PDF :(")
     # if the GET request did not succeed
     else:
-        print("Could not open this file :(")
+      print("Could not open this file :(")
     return content_type, to_download
+  except MissingSchema:
+    return "invalid", False
+
 
 # introduction
-print("Welcome, I'm your file download helper. For now, I am only able to help you download open access PDFs!")
+print(
+  "Welcome, I'm your file download helper. For now, I am only able to help you download open access PDFs!"
+)
 
 # user input for links
-links_string = input("Paste in links to files, each link separated by a comma. Hit 'Enter' once you're done\n")
+links_string = input(
+  "Paste in links to files, each link separated by a comma. Hit 'Enter' once you're done\n"
+)
 
 # separating links and storing in a list
 links = links_string.split(",")
@@ -46,24 +56,25 @@ links = links_string.split(",")
 # for each link in the list
 count = 0
 for link in links:
-    count += 1
-    to_download = False
-    print("~~~~~")
-    print(str(count) + ". " + link)
-    # checking whether link has already been processed before by searching in db
-    if mongoservice.exists(link):
-        # update
-        content_type, to_download = process(link)
-        mongoservice.update(link, {
-            "downloaded": to_download,
-            "link_type": content_type
-        })
-    else:
-        # download
-        content_type, to_download = process(link)
-        # insert link into db to keep track of what has been processed already
-        mongoservice.insert({
-            "link": link,
-            "downloaded": to_download,
-            "link_type": content_type
-        })
+  count += 1
+  to_download = False
+  print("~~~~~")
+  print(str(count) + ". " + link)
+  # checking whether link has already been processed before by searching in db
+  content_type, to_download = process(link)
+  if content_type == "invalid":
+    print("Invalid link: " + link)
+    continue
+  if mongoservice.exists(link):
+    # update
+    mongoservice.update(link, {
+      "downloaded": to_download,
+      "link_type": content_type
+    })
+  else:
+    # insert link into db to keep track of what has been processed already
+    mongoservice.insert({
+      "link": link,
+      "downloaded": to_download,
+      "link_type": content_type
+    })
--- mongoservice.py
+++ mongoservice.py
@@ -1,21 +1,24 @@
-import pymongo
-import ssl
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
 
+# connecting to (mongodb.com) database
 mongodb_url = "mongodb+srv://varshav0119:{password}@dev.i0hulwp.mongodb.net/?retryWrites=true&w=majority"
-client = pymongo.MongoClient(mongodb_url.format(password = "postcanvasdb"), tlsAllowInvalidCertificates = True)
+client = MongoClient(mongodb_url.format(password="postcanvasdb"),
+                     server_api=ServerApi('1'),
+                     tlsAllowInvalidCertificates=True)
 
 db = "pdfs"
 collection = "links"
 
 def insert(document):
-    return client[db][collection].insert_one(document)
+  return client[db][collection].insert_one(document)
 
 def update(link, document):
-    client[db][collection].update_one({"link": link}, { "$set": document })
+  client[db][collection].update_one({"link": link}, {"$set": document})
 
 def exists(link):
-    record = client[db][collection].find_one({"link": link})
-    if record:
-        return True
-    else:
-        return False
+  record = client[db][collection].find_one({"link": link})
+  if record:
+    return True
+  else:
+    return False
